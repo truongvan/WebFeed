a08b7b3ab9c6deaa6aae80b87e8eea6cefed71f7
truongvan <truongvan@live.com>
2024-02-17 17:48:15 +0700

    feat: Save store after set value

--- src/lib/settings/store.ts
+++ src/lib/settings/store.ts
@@ -1,39 +1,37 @@
 import { writable } from "svelte/store";
 import { Store } from "@tauri-apps/plugin-store";
 interface Settings {
-    workingFolder: string | null
+    workingFolder: string | null;
 }
 const store = new Store("settings.dat");
 
 function createWritable() {
-    const KEY_STORE = "settings"
+    const KEY_STORE = "settings";
     const { subscribe, set, update } = writable<Settings>({
-        workingFolder: null
-    })
-    let unsubscribe = () => { }
+        workingFolder: null,
+    });
+    let unsubscribe = () => {};
 
-    function load() {
-        store.get<Settings>(KEY_STORE).then((value) => {
-            if (value) {
-                set(value)
-            }
-        })
+    async function load() {
+        let value = await store.get<Settings>(KEY_STORE);
+        if (value) {
+            set(value);
+        }
 
         unsubscribe = subscribe((value) => {
             if (value) {
                 store.set(KEY_STORE, value);
+                store.save();
             }
-
-
-        })
-    }
-    function unload() {
-        unsubscribe()
+        });
     }
 
     return {
-        subscribe, set, update, unload, load
-    }
-
+        subscribe,
+        set,
+        update,
+        load,
+        unsubscribe,
+    };
 }
-export const settingsStore = createWritable()
+export const settingsStore = createWritable();
