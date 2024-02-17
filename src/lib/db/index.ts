3e3cfb92ad7af0ba313e6b9c38a9305ec89b5d16
truongvan <truongvan@live.com>
2024-02-17 17:47:27 +0700

    feat: Throw error if working folder is null

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,17 +2,17 @@ import { settingsStore } from "$lib/settings/store";
 import Database from "@tauri-apps/plugin-sql";
 import { get } from "svelte/store";
 
-let db_: Database | null = null
+let db_: Database | null = null;
 export async function getDB() {
     if (db_) {
-        return db_
+        return db_;
     }
 
     let { workingFolder } = get(settingsStore);
-    console.log(workingFolder);
+    if (workingFolder === null) {
+        throw new Error("Working folder is not set, can not initial database.");
+    }
 
-    db_ = await Database.load(
-        `sqlite:${workingFolder}/metadata.db`
-    );
-    return db_
+    db_ = await Database.load(`sqlite:${workingFolder}/metadata.db`);
+    return db_;
 }
